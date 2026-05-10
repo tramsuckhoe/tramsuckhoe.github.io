b7c95b1aaa731b384802a7360f51c87b216fb73a
tramsuckhoe <dovanloi1799@gmail.com>
2026-05-10 22:30:32 +0700

    init commit

---
+++ data.js
@@ -0,0 +1,27 @@
+const pageData = {
+  "profile": {
+    "name": "Trạm Sức Khỏe Gia Đình",
+    "bio": "Giải pháp chăm sóc sức khỏe chủ động và an toàn cho mọi thành viên trong nhà.",
+    "avatar": "https://scontent.fhan2-3.fna.fbcdn.net/v/t39.30808-1/690608414_122099828265298312_7386649520751486947_n.jpg?stp=c320.0.768.768a_dst-jpg_s200x200_tt6&_nc_cat=108&ccb=1-7&_nc_sid=2d3e12&_nc_ohc=Yt8y0u3JpSoQ7kNvwEhKPKx&_nc_oc=AdpdNW0CIRyUW1ZPUai7LAFtxKfbXWDbM7FGSa8mjg7ckDOSZkfcZ4s_jFLGnaWkWIi9yquiMwMCtie6k9PUeI2B&_nc_zt=24&_nc_ht=scontent.fhan2-3.fna&_nc_gid=r3qjFNOcuhdjMbATuoxmhg&_nc_ss=7b2a8&oh=00_Af4djOBseuEYRjRvPZLJ-xSQ6co5iXH9P_lWb03Csmm1Dw&oe=6A066C8F",
+    "socials": [
+      { "name": "Facebook", "icon": "fa-facebook", "url": "https://www.facebook.com/tramsuckhoe.giadinh", "color": "text-blue-500 hover:text-blue-400" },
+      { "name": "TikTok", "icon": "fa-tiktok", "url": "https://www.tiktok.com/@tramsuckhoe.giadinh", "color": "text-slate-800 hover:text-slate-600 dark:text-white" },
+      { "name": "Shopee", "icon": "fa-bag-shopping", "url": "https://s.shopee.vn/8piVUIT4GF", "color": "text-orange-500 hover:text-orange-400" }
+    ]
+  },
+  "categoriesConfig": [
+    {
+      "id": "thiet-bi",
+      "title": "Tủ thuốc & Thiết bị",
+      "icon": "fa-stethoscope",
+      "color": "text-cyan-100"
+    },
+    {
+      "id": "thuc-pham",
+      "title": "Dinh dưỡng & Đề kháng",
+      "icon": "fa-shield-virus",
+      "color": "text-emerald-100"
+    }
+  ],
+  "csvUrl": "https://docs.google.com/spreadsheets/d/e/2PACX-1vSfwg4CNm_f2uxYjs--A2J-Ja5PPLplmk4RFY2Hj7HtqKXUoqqnkjndWkGQ_6_9wyKkdCst1oGTBik5/pub?output=csv"
+};
