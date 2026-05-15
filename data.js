49447c6427e280016cc2531194b3dce21ae93381
tramsuckhoe <dovanloi1799@gmail.com>
2026-05-16 02:38:06 +0700

    Add files via upload

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ const pageData = {
   "profile": {
     "name": "Trạm Sức Khỏe Gia Đình",
     "bio": "Giải pháp chăm sóc sức khỏe chủ động và an toàn cho mọi thành viên trong nhà.",
-    "avatar": "https://scontent.fhan2-3.fna.fbcdn.net/v/t39.30808-1/690608414_122099828265298312_7386649520751486947_n.jpg?stp=c320.0.768.768a_dst-jpg_s200x200_tt6&_nc_cat=108&ccb=1-7&_nc_sid=2d3e12&_nc_ohc=Yt8y0u3JpSoQ7kNvwEhKPKx&_nc_oc=AdpdNW0CIRyUW1ZPUai7LAFtxKfbXWDbM7FGSa8mjg7ckDOSZkfcZ4s_jFLGnaWkWIi9yquiMwMCtie6k9PUeI2B&_nc_zt=24&_nc_ht=scontent.fhan2-3.fna&_nc_gid=r3qjFNOcuhdjMbATuoxmhg&_nc_ss=7b2a8&oh=00_Af4djOBseuEYRjRvPZLJ-xSQ6co5iXH9P_lWb03Csmm1Dw&oe=6A066C8F",
+    "avatar": "https://tramsuckhoe.github.io/avatar.jpg",
     "socials": [
       { "name": "Facebook", "icon": "fa-facebook", "url": "https://www.facebook.com/tramsuckhoe.giadinh", "color": "text-blue-500 hover:text-blue-400" },
       { "name": "TikTok", "icon": "fa-tiktok", "url": "https://www.tiktok.com/@tramsuckhoe.giadinh", "color": "text-slate-900 hover:text-slate-700" },
